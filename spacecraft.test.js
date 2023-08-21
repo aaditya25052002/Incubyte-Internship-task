a233002ddcbc411fdc33a8c96206453f3c045d49
Aditya Tiwari <pubgmlord25@gmail.com>
2023-08-22 01:36:18 +0530

    Added failing test for forward movement.

--- spacecraft.test.js
+++ spacecraft.test.js
@@ -7,3 +7,10 @@ test("initializes spacecraft with correct position and direction", () => {
   expect(spacecraft.z).toBe(0);
   expect(spacecraft.direction).toBe("N");
 });
+
+test('moves spacecraft forward when direction is N', () => {
+    const spacecraft = new Spacecraft(0, 0, 0, 'N');
+    spacecraft.move('f');
+    expect(spacecraft.y).toBe(1);
+});
+
