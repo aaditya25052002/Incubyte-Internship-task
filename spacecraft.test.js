d8354c9577a4b907174bb25e663632bf5987ada0
Aditya Tiwari <pubgmlord25@gmail.com>
2023-08-22 09:41:47 +0530

    Added failing tests for other movement and turning commands.

--- spacecraft.test.js
+++ spacecraft.test.js
@@ -14,3 +14,33 @@ test('moves spacecraft forward when direction is N', () => {
     expect(spacecraft.y).toBe(1);
 });
 
+test('moves spacecraft backward when direction is N', () => {
+    const spacecraft = new Spacecraft(0, 0, 0, 'N');
+    spacecraft.move('b');
+    expect(spacecraft.y).toBe(-1);
+});
+
+test('turns spacecraft right from N direction', () => {
+    const spacecraft = new Spacecraft(0, 0, 0, 'N');
+    spacecraft.move('r');
+    expect(spacecraft.direction).toBe('E');
+});
+
+test('turns spacecraft left from N direction', () => {
+    const spacecraft = new Spacecraft(0, 0, 0, 'N');
+    spacecraft.move('l');
+    expect(spacecraft.direction).toBe('W');
+});
+
+test('turns spacecraft up from N direction', () => {
+    const spacecraft = new Spacecraft(0, 0, 0, 'N');
+    spacecraft.move('u');
+    expect(spacecraft.direction).toBe('U');
+});
+
+test('turns spacecraft down from N direction', () => {
+    const spacecraft = new Spacecraft(0, 0, 0, 'N');
+    spacecraft.move('d');
+    expect(spacecraft.direction).toBe('D');
+});
+
