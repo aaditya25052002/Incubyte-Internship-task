b1c0c962c4377fef50dc78e97622cfa4fbc3f27c
Aditya Tiwari <pubgmlord25@gmail.com>
2023-08-23 22:29:19 +0530

    Refactored the code and added Readme file

---
+++ readme.md
@@ -0,0 +1,83 @@
+
+
+# Chandrayaan 3 Lunar Craft: Galactic Space Craft Control
+
+A CLI application to navigate a spacecraft in 3D space. The user defines the spacecraft's initial direction and provides a sequence of commands to navigate it. The program will then return the spacecraft's final position and direction.
+
+## Table of Contents
+
+- [Getting Started](#getting-started)
+- [Prerequisites](#prerequisites)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Running the tests](#running-the-tests)
+- [Built With](#built-with)
+- [Authors](#authors)
+
+---
+
+## Getting Started
+
+Follow the instructions to get the project running on your local machine for development and testing.
+
+---
+
+## Prerequisites
+
+- [Node.js](https://nodejs.org/) and NPM
+
+---
+
+## Installation
+
+1. Clone the repository:
+    ```
+    git clone https://github.com/yourusername/spacecraft-navigation-cli.git
+    ```
+
+2. Navigate to the project directory:
+    ```
+    cd spacecraft-navigation-cli
+    ```
+
+3. Install the required packages:
+    ```
+    npm install
+    ```
+
+---
+
+## Usage
+
+1. Run the program:
+    ```
+    node index.js
+    ```
+
+2. Follow the on-screen prompts to set the spacecraft's initial direction and provide navigation commands.
+
+3. The program will output the final position and direction of the spacecraft.
+
+---
+
+## Running the tests
+
+To ensure the reliability of the navigation system, unit tests are provided. To run these tests:
+    ```
+    npm test
+    ```
+
+---
+
+## Built With
+
+- [Node.js](https://nodejs.org/) - The runtime used
+- Jest (or any other testing library you're using) - Testing framework
+
+---
+
+## Authors
+
+- [Aditya Tiwari](https://github.com/aaditya25052002) - Initial work
+
+---
--- spacecraft.js
+++ spacecraft.js
@@ -1,85 +1,99 @@
+/**
+ * Represents a spacecraft capable of moving in a three-dimensional space.
+ */
 class Spacecraft {
+    /**
+     * Creates a new spacecraft instance.
+     * 
+     * @param {number} x - Initial x-coordinate.
+     * @param {number} y - Initial y-coordinate.
+     * @param {number} z - Initial z-coordinate.
+     * @param {string} direction - Initial direction ('N', 'E', 'S', 'W', 'U', 'D').
+     */
     constructor(x, y, z, direction) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
-        this.direction = direction;
+      this.x = x;
+      this.y = y;
+      this.z = z;
+      this.direction = direction;
+  
+      // Defines movement vectors for each direction.
+      this.DIRECTION_MATRIX = {
+        N: { x: 0, y: 1, z: 0 },  // North
+        E: { x: 1, y: 0, z: 0 },  // East
+        S: { x: 0, y: -1, z: 0 }, // South
+        W: { x: -1, y: 0, z: 0 }, // West
+        U: { x: 0, y: 0, z: 1 },  // Up
+        D: { x: 0, y: 0, z: -1 }  // Down
+      };
+  
+      // Defines the order of directions for turning purposes.
+      this.DIRECTION_ORDER = ["N", "E", "S", "W"];
     }
+  
+    /**
+     * Moves the spacecraft based on the provided command.
+     * 
+     * @param {string} command - Movement command ('f', 'b', 'l', 'r', 'u', 'd').
+     */
     move(command) {
-        switch (command) {
-            case 'f':
-                this.forward();
-                break;
-            case 'b':
-                this.backward();
-                break;
-            case 'l':
-                this.turnLeft();
-                break;
-            case 'r':
-                this.turnRight();
-                break;
-            case 'u':
-                this.turnUp();
-                break;
-            case 'd':
-                this.turnDown();
-                break;
-        }
+      switch (command) {
+        case "f":
+          this.advance(1);
+          break;
+        case "b":
+          this.advance(-1);
+          break;
+        case "l":
+          this.turn(-1);  // Turn left
+          break;
+        case "r":
+          this.turn(1);   // Turn right
+          break;
+        case "u":
+          this.direction = "U";  // Turn upwards
+          break;
+        case "d":
+          this.direction = "D";  // Turn downwards
+          break;
+      }
     }
-    
-    forward() {
-        switch (this.direction) {
-            case 'N': this.y++; break;
-            case 'E': this.x++; break;
-            case 'S': this.y--; break;
-            case 'W': this.x--; break;
-            case 'U': this.z++; break;
-            case 'D': this.z--; break;
-        }
+  
+    /**
+     * Turns the spacecraft in a given direction.
+     * 
+     * @param {number} turnDirection - Indicates the direction to turn. 1 for right and -1 for left.
+     */
+    turn(turnDirection) {
+      if (this.direction === "U") {
+        this.direction = turnDirection === 1 ? "S" : "N";
+        return;
+      }
+      if (this.direction === "D") {
+        this.direction = turnDirection === 1 ? "N" : "S";
+        return;
+      }
+  
+      const currentIdx = this.DIRECTION_ORDER.indexOf(this.direction);
+      const newIdx = (currentIdx + turnDirection + 4) % 4;
+      this.direction = this.DIRECTION_ORDER[newIdx];
     }
-    
-    backward() {
-        switch (this.direction) {
-            case 'N': this.y--; break;
-            case 'E': this.x--; break;
-            case 'S': this.y++; break;
-            case 'W': this.x++; break;
-            case 'U': this.z--; break;
-            case 'D': this.z++; break;
-        }
+  
+    /**
+     * Advances the spacecraft in its current direction.
+     * 
+     * @param {number} multiplier - Represents the distance and direction to advance.
+     */
+    advance(multiplier) {
+      const directionValues = this.DIRECTION_MATRIX[this.direction];
+      if (!directionValues) {
+        console.error(`Invalid direction: ${this.direction}`);
+        return;
+      }
+      this.x += directionValues.x * multiplier;
+      this.y += directionValues.y * multiplier;
+      this.z += directionValues.z * multiplier;
     }
-    
-    turnLeft() {
-        switch (this.direction) {
-            case 'N': this.direction = 'W'; break;
-            case 'E': this.direction = 'N'; break;
-            case 'S': this.direction = 'E'; break;
-            case 'W': this.direction = 'S'; break;
-            case 'U': this.direction = 'N'; break;  // Orientation change when facing Up
-            case 'D': this.direction = 'S'; break;  // Orientation change when facing Down
-        }
-    }
-    
-    turnRight() {
-        switch (this.direction) {
-            case 'N': this.direction = 'E'; break;
-            case 'E': this.direction = 'S'; break;
-            case 'S': this.direction = 'W'; break;
-            case 'W': this.direction = 'N'; break;
-            case 'U': this.direction = 'S'; break;  // Orientation change when facing Up
-            case 'D': this.direction = 'N'; break;  // Orientation change when facing Down
-        }
-    }
-    
-    turnUp() {
-        this.direction = 'U';
-    }
-    
-    turnDown() {
-        this.direction = 'D';
-    }
-       
-}
-
-module.exports = Spacecraft;
+  }
+  
+  module.exports = Spacecraft;
+  
