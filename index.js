414946b44d2f06fb4de63960bd5e631c7837b155
Aditya Tiwari <pubgmlord25@gmail.com>
2023-08-23 22:56:27 +0530

    minor checks

--- index.js
+++ index.js
@@ -1,33 +1,41 @@
 // Required modules
-const readline = require('readline');
-const Spacecraft = require('./spacecraft.js');
+const readline = require("readline");
+const Spacecraft = require("./spacecraft.js");
 
 // Setup CLI interface
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout,
 });
 
 // Get initial direction for the spacecraft
-rl.question('Initial direction (N, S, E, W, U, D): ', (initialDirection) => {
-    
-    // Get movement commands after setting direction
-    rl.question('Commands (e.g., f,r,u,b,l): ', (input) => {
-        
-        // Parse and trim command input
-        const commands = input.split(',').map(command => command.trim());
+rl.question("Initial direction (N, S, E, W, U, D): ", (initialDirection) => {
+  if (
+    !["N", "S", "E", "W", "U", "D"].includes(initialDirection.toUpperCase())
+  ) {
+    console.log(`Invalid direction: ${initialDirection}. Exiting.`);
+    rl.close();
+    return;
+  }
 
-        // Initialize spacecraft at origin with user-defined direction
-        const spacecraft = new Spacecraft(0, 0, 0, initialDirection);
+  // Get movement commands after setting direction
+  rl.question("Commands (e.g., f,r,u,b,l): ", (input) => {
+    // Parse and trim command input
+    const commands = input.split(",").map((command) => command.trim());
 
-        // Process each command
-        commands.forEach(command => spacecraft.move(command));
+    // Initialize spacecraft at origin with user-defined direction
+    const spacecraft = new Spacecraft(0, 0, 0, initialDirection);
 
-        // Output final position and direction
-        console.log(`Position: (${spacecraft.x}, ${spacecraft.y}, ${spacecraft.z})`);
-        console.log(`Direction: ${spacecraft.direction}`);
+    // Process each command
+    commands.forEach((command) => spacecraft.move(command));
 
-        // Terminate program
-        rl.close();
-    });
+    // Output final position and direction
+    console.log(
+      `Position: (${spacecraft.x}, ${spacecraft.y}, ${spacecraft.z})`
+    );
+    console.log(`Direction: ${spacecraft.direction}`);
+
+    // Terminate program
+    rl.close();
+  });
 });
